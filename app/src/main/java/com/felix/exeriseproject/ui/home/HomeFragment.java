e5390b1abbd26271e5cf1778d01f1ed2247501f1
Tollin <tollinxu@gmail.com>
2020-05-20 15:42:41 +1200

    home page

--- app/src/main/java/com/felix/exeriseproject/ui/home/HomeFragment.java
+++ app/src/main/java/com/felix/exeriseproject/ui/home/HomeFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,24 +13,58 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.felix.exeriseproject.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
-
+    private static String url ="https://api.covid19api.com/summary";
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+//        homeViewModel =
+//                ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        final TextView tvNewConfirm = root.findViewById(R.id.tvNewConfirm);
+//        final TextView textView = root.findViewById(R.id.tvHeader);
+//        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
+        RequestQueue queue = Volley.newRequestQueue(getContext());
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                       // textView.setText("Response is: "+ response.substring(0,500));
+                        try {
+                            JSONObject jobject= new JSONObject(response);
+                            Object global = jobject.getJSONObject("Global").get("NewConfirmed");
+                            tvNewConfirm.setText(global.toString());
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getContext(), error.getMessage(), Toast.LENGTH_LONG).show();
             }
         });
+
+        queue.add(stringRequest);
         return root;
     }
 }
--- app/src/main/java/com/felix/exeriseproject/ui/home/HomeViewModel.java
+++ app/src/main/java/com/felix/exeriseproject/ui/home/HomeViewModel.java
@@ -4,6 +4,9 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
 public class HomeViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
@@ -11,6 +14,7 @@ public class HomeViewModel extends ViewModel {
     public HomeViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is home fragment");
+        //RequestQueue queue = Volley.newRequestQueue(this);
     }
 
     public LiveData<String> getText() {
