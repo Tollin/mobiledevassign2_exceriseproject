fa5295c1e8114b6b784c8e5cd91c23cd380cce27
Tollin <tollinxu@gmail.com>
2020-05-21 00:37:04 +1200

    finish excersice 5

--- app/src/main/java/com/felix/exeriseproject/MainActivity.java
+++ app/src/main/java/com/felix/exeriseproject/MainActivity.java
@@ -1,13 +1,9 @@
 package com.felix.exeriseproject;
-
 import android.os.Bundle;
-import android.view.View;
 import android.view.Menu;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
---
+++ app/src/main/java/com/felix/exeriseproject/ui/map/MapFragment.java
@@ -0,0 +1,83 @@
+package com.felix.exeriseproject.ui.map;
+
+import android.app.Activity;
+import android.location.Location;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.felix.exeriseproject.MainActivity;
+import com.felix.exeriseproject.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+
+public class MapFragment extends Fragment {
+
+    MapView mMapView;
+    private GoogleMap googleMap;
+    private ImageButton btnLocate;
+    private FusedLocationProviderClient fusedLocationClient;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View root = inflater.inflate(R.layout.fragment_map, container, false);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
+        SupportMapFragment mapFragment =  (SupportMapFragment)getChildFragmentManager()
+                .findFragmentById(R.id.incident_map);
+        btnLocate = root.findViewById(R.id.btnLocate);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMapInstance) {
+                googleMap = googleMapInstance;
+            }
+        });
+        btnLocate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                        fusedLocationClient.getLastLocation()
+                                .addOnSuccessListener(new OnSuccessListener<Location>() {
+                                    @Override
+                                    public void onSuccess(Location location) {
+                                        if(location != null){
+                                            LatLng myplce = new LatLng(location.getLatitude(), location.getLongitude());
+                                            googleMap.addMarker(new MarkerOptions().position(myplce).title("This is my home"));
+                                            googleMap.moveCamera(CameraUpdateFactory.newLatLng(myplce));
+//                                            tvLatitude.setText(String.valueOf(location.getLatitude()));
+//                                            tvLongtitude.setText(String.valueOf(location.getLongitude()));
+                                        }else {
+                                            Toast.makeText(getContext(), "don't retrieve any location", Toast.LENGTH_LONG).show();
+                                        }
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+                                    }
+                                });
+                    }
+                });
+        return root;
+    }
+
+}
